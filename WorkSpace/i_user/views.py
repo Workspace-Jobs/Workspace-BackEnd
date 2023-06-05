22f6ad8f820b9d7f362e35d0cc664073b6a14b8c
정재훈 <81303380+nasnaky@users.noreply.github.com>
2023-06-05 18:04:19 +0900

    공고 검색 오류 해결

--- WorkSpace/i_user/views.py
+++ WorkSpace/i_user/views.py
@@ -318,7 +318,7 @@ class EMPLOYMENTSearchList(APIView):
     def get(self, request):
         search = request.query_params['search']
         EM_O = EMPLOYMENT.objects.filter(
-            Q(title__contains=search) | Q(job__contains=search) | Q(user__username__contains=search) | Q(
+            Q(title__contains=search) | Q(job__contains=search) | Q(user__username__contains=search) and Q(
                 date__gte=date.today())).order_by('-id')
         paginator = self.pagination_class()
         result_page = paginator.paginate_queryset(EM_O, request)
